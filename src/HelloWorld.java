a8f33eb599031ac8a58f2c34a3713f1a8b818509
Christina Nuncio <nuncio.christina01@gmail.com>
2021-09-03 10:31:17 -0500

    JAVA exercise stinrgs n values

---
+++ src/HelloWorld.java
@@ -0,0 +1,70 @@
+public class HelloWorld {
+        public static void main(String[] args) {
+//           int myFavoriteNumber = 23;
+//            System.out.println(myFavoriteNumber);
+
+//            String myString = "My fav num is 23.";
+//            System.out.println(myString);
+
+//            long myFavoriteNumber;
+//           myFavoriteNumber = 123;
+
+//           float myFavoriteNumber = 3.14F;
+
+//            int x = 5;
+//            System.out.println(x++);
+//            System.out.println(x);
+//5
+            //6
+
+//            int x = 5;
+//            System.out.println(++x);
+//            System.out.println(x);
+//6
+            //6
+
+//            String class = "don't do this";
+
+//            String theNumberThree = "three";
+//            Object o = theNumberThree;
+//            int three = (int) o;
+
+//            int three = (int) "three";
+
+//            int x = 4;
+//            x = x + 5;
+
+//shorthand:
+
+//            int x = 4;
+//            x += 5;
+
+//            int x = 3;
+//            int y = 4;
+//            y = y * x;
+
+//shorthand:
+
+//            int x = 3;
+//            int y = 4;
+//            y *= x;
+
+//            int x = 10;
+//            int y = 2;
+//            x = x / y;
+//            y = y - x;
+//
+//            //for
+//
+//            int x = 10;
+//            int y = 2;
+//            x /= y;
+//            y -= x;
+
+            //
+                System.out.println(Integer.MAX_VALUE);
+                int testInteger = 2147483647;
+                System.out.println(++testInteger);
+
+        }
+}
