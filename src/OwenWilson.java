2b1563013686413c85bfbb393f9dfcb2e9d3ccdf
Christina Nuncio <nuncio.christina01@gmail.com>
2021-09-10 12:31:08 -0500

    JAVA method and classes

---
+++ src/OwenWilson.java
@@ -0,0 +1,12 @@
+public class OwenWilson {
+    public static String name;
+    public static String sound;
+
+
+public void makeNoise() {
+    System.out.println(Quote.generateQuote());
+    System.out.println(name + " makes the sound: " + "'WOW.' " + sound);
+    }
+}
+//    Create a class for your favorite type of animal. The class should be named for your animal - Cat, Dog, Parrot etc. It should have two instance variables - name and sound - and one method - makeNoise(). The output of the makeNoise method should be name +  " goes " + sound. Create a test class with a main method to instantiate and test your class code.
+
---
+++ src/OwenWilsonTest.java
@@ -0,0 +1,12 @@
+public class OwenWilsonTest {
+
+    public static void main(String[] args) {
+        OwenWilson owenWilson = new OwenWilson();
+        owenWilson.name = "Owen Wilson ";
+        owenWilson.sound = "Wow. ";
+        owenWilson.makeNoise();
+    }
+}
+
+
+
