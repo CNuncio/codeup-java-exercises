b92d572f73659312e37c19921fb4e8466b1842ab
Christina Nuncio <nuncio.christina01@gmail.com>
2021-09-17 17:10:16 -0500

    implementations and interfaces

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,27 +1,27 @@
 package shapes;
+//
+//public class Square extends Rectangle {
 
-public class Square extends Rectangle {
-
-    private double side;
-
-    public Square (double side){
-super(side, side);
-this.side = side;
-
-    }
-
-    public double getArea(){
-        System.out.println("square ga method");
-
-        return side * side;
-    }
-
-    public double getPerimeter(){
-        System.out.println("square gp method");
-
-        return side * 4;
-
-    }
+//    private double side;
+//
+//    public Square (double side){
+//super(side, side);
+//this.side = side;
+//
+//    }
+//
+//    public double getArea(){
+//        System.out.println("square ga method");
+//
+//        return side * side;
+//    }
+//
+//    public double getPerimeter(){
+//        System.out.println("square gp method");
+//
+//        return side * 4;
+//
+//    }
 
 
-}
+//}
