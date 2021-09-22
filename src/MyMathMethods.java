b49684cd0c4f4afb7deeb5a9f04344c62dbf55b8
Christina Nuncio <nuncio.christina01@gmail.com>
2021-09-22 10:12:53 -0500

    overloading mathmethods

--- src/MyMathMethods.java
+++ src/MyMathMethods.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class MyMathMethods {
-       public int multiply (int num1, int num2) {
+       public static int multiply (int num1, int num2) {
 
-              return num1 * num2;
-
+              {return num1 * num2;}
+// use method overloading to create a public static multiply method that works with doubles
+              public static double multiply (int num1, int num2){return num1 * num2;}
        }
 
 }
--- src/MyMathMethodsTest.java
+++ src/MyMathMethodsTest.java
@@ -1,4 +1,7 @@
 public class MyMathMethodsTest {
+
+    public static int mathTestInt = -23
+            public static int absoluteValueTest = Math.abs(mathTestInt);
     public static void main(String[] args) {
         MyMathMethods mathMethods = new MyMathMethods();
 //        System.out.println(mathMethods.multiply(2, 3));
