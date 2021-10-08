c69d7e4498d048ffcb3e804145101ea0574c0898
Christina Nuncio <nuncio.christina01@gmail.com>
2021-10-08 08:59:48 -0500

    updates

---
+++ src/HighLow.java
@@ -0,0 +1,37 @@
+public class HighLow {
+
+
+    public static boolean printGuessMessage(int guess, int randomNum) {
+        boolean willKeepGoing = true;
+        if (guess == randomNum) {
+            System.out.println("GOOD GUESS!");
+            willKeepGoing = false;
+        } else if (guess < randomNum) {
+            System.out.println("HIGHER");
+        } else {
+            System.out.println("LOWER");
+        }
+        return willKeepGoing;
+    }
+
+    public static void runGuesses(int randomNum) {
+        System.out.println("Guess a number between 1 and 100");
+        int guess = MethodsExercises.getInteger(1, 100);
+
+        if (printGuessMessage(guess, randomNum)) {
+            runGuesses(randomNum);
+        }
+    }
+
+//    public static void init() {
+//        System.out.println("Welcome to the High / Low Game!");
+//        int randomNum = MethodsExer.getRandInt(1, 100);
+//        System.out.println(randomNum); // FOR DEBUGGING
+//        runGuesses(randomNum);
+//    }
+
+    public static void main(String[] args) {
+        init();
+    }
+
+}
