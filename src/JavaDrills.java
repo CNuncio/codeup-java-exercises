54ea0cf0aa3f7758e90f6ced5b73534a9eef9219
Christina Nuncio <nuncio.christina01@gmail.com>
2021-10-14 09:33:04 -0500

    JAVA drills flip first and last letters

---
+++ src/JavaDrills.java
@@ -0,0 +1,25 @@
+public class JavaDrills {
+
+    public static String flipOuterCase(String input) {
+
+        for (int i = 0, i <chars.length;
+        i++){
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+//work in progress
+
+
+//        In `codeup-java-exercises`, create a class `JavaDrills`, with the following:
+//        Create a public static method, `flipOuterCase`, that takes in a string and returns the string
+//        with the case reversed for the first and last letters. Assume the input will be three or more characters long.
+//        Example inputs and expected outputs...
+//        System.out.println(flipOuterCase(“cat”)); // CaT
+//        System.out.println(flipOuterCase(“CaT”)); // cat
+//        System.out.println(flipOuterCase(“caT”)); // Cat
+//        System.out.println(flipOuterCase(“cAt”)); // CAT
