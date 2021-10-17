1f696fddffa7ad83d3b2b87dca9686fbda488464
Christina Nuncio <nuncio.christina01@gmail.com>
2021-10-16 22:24:17 -0500

    blackFridaySale issue

---
+++ src/blackFridaySale.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class blackFridaySale {
+    private static ArrayList<Device> Device;
+    public class static <Device> ArrayList<Device> blackFriday(ArrayList<Device> obj) {
+        boolean brand;
+        if(brand == "Apple") {
+
+            return (Device * .05);
+        }
+        else if (brand == "Microsoft") {
+
+        }
+        return (Device * .07);
+    }
+}
+
+// work in progress
+// create a static method named blackFridaySale. This method should:
+//        Take in an ArrayList of Device objects
+//        The method should return an ArrayList of Device objects where each device's price is lowered with a discount based on the following rules:
+//        If the device brand is Apple it gets a 5% off on the normal price.
+//        If the device brand is Microsoft it gets a 7% off on the normal price;
